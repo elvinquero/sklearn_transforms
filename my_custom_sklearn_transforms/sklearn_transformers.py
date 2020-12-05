f01a1695e92f6f947397d687ae50a239d78281f5
Elvin Quero <elvinempor@gmail.com>
2020-12-05 13:38:30 -0400

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,4 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import MinMaxScaler
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -14,3 +15,23 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+class KoiScore(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.scaler =  MinMaxScaler()
+    
+    def fit(self, X, y=None):
+        df_koi_normalizado =  X[ X['koi_score'] <= 1 ]
+        df_koi_por_normalizar =  X[ X['koi_score'] > 1]
+        
+        self.scaler.fit(df_koi_por_normalizar['koi_score'])
+        return X
+    
+    def transform(self, X):
+        df_koi_normalizado =  X[ X['koi_score'] <= 1 ]
+        df_koi_por_normalizar =  X[ X['koi_score'] > 1]
+        df_koi_por_normalizar['koi_score'] = self.scaler.transform(df_koi_por_normalizar['koi_score'])
+        return pd.concat([df_koi_normalizado, df_koi_por_normalizar])
+        
+        
+        
